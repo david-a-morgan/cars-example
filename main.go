8ff4cd6302d304353551108f30bd7b8146de84e3
David Morgan <david.morgan@appian.com>
2022-09-20 13:43:31 -0400

    Manifests

--- main.go
+++ main.go
@@ -115,7 +115,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	setupLog.Info("starting manager")
+	setupLog.WithValues("color", color).Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
